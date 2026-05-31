90e8f90d9c1675dce4df88d694203587754e1b46
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-05-31 14:34:34 +0530

    Removed retention of history

--- src/app.py
+++ src/app.py
@@ -6,27 +6,20 @@ from .resources.llm_prompt import prompt
 
 app = FastAPI()
 
-message_history = [
-    {
-        "role" : "system",
-        "content" : prompt
-    }
-]
-
 @app.post("/generate-comment")
 async def generate_comment(comment_text: CommentText):
     try: 
-        message = {
+        message = [{
+                "role" : "system",
+                "content" : prompt
+                },
+                {
                 "role" : "user",
                 "content" : comment_text.comment
-                }
-
-        message_history.append(message)
-        system_message = message_history[0]
-        recent_messages = message_history[-9:]
-        payload_message = [system_message, *recent_messages]
-
-        generated_comment = await llm_generate_comment(messages=payload_message)
+                }]
+        
+        generated_comment = await llm_generate_comment(messages=message)
+        
         return {"generated_comment" : generated_comment}
     except Exception as e:
         logger.error(f"Error occured: {e}")
